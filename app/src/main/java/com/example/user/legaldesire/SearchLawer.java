16caa6c6259e2f75382f97c39022c39d1c0cb7c9
dashsonali <sonali031998@gmail.com>
2018-12-26 21:36:08 +0530

    "ui"

--- app/src/main/java/com/example/user/legaldesire/SearchLawer.java
+++ app/src/main/java/com/example/user/legaldesire/SearchLawer.java
@@ -2,12 +2,40 @@ package com.example.user.legaldesire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SearchLawer extends AppCompatActivity {
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_lawer);
+        imageButton=findViewById(R.id.imageBtn);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(getApplicationContext(),imageButton);
+                popup.getMenuInflater()
+                        .inflate(R.menu.filter, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                       /* Toast.makeText(
+                                MainActivity.this,
+                                "You Clicked : " + item.getTitle(),
+                                Toast.LENGTH_SHORT
+                        ).show();*/
+                        return true;
+                    }
+            });
+
+                popup.show(); //showing popup menu
+
+
     }
-}
+});
+    }}

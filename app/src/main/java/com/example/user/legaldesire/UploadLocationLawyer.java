b9eebf3cbc067bddb6583e08dad04b6894978f25
rakeswain <swain.rakesh131@gmail.com>
2018-12-26 11:37:41 +0530

    Map Initialised!

--- app/src/main/java/com/example/user/legaldesire/UploadLocationLawyer.java
+++ app/src/main/java/com/example/user/legaldesire/UploadLocationLawyer.java
@@ -3,89 +3,164 @@ package com.example.user.legaldesire;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UploadLocationLawyer extends AppCompatActivity {
-
-   private static final int LOCATION_PERMISSION_REQUEST = 1234;
+public class UploadLocationLawyer extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private String[] permissoins = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    private static final int LOCATION_PERMISSION_REQUEST = 1234;
     private Button uploadLocation;
     private static boolean PERMISSION_GRANTED = false;
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiCleint;
+    LocationRequest mLocationRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_location_lawyer);
-        uploadLocation = findViewById(R.id.uploadLocation);
-        getLocationPermission();
+        //  uploadLocation = findViewById(R.id.uploadLocation);
+        initMap();
 
     }
 
-    private  void initMap(){
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                mMap = googleMap;
-            }
-        });
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
     }
-   private void getLocationPermission(){
-        String[] permissoins = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)==
-                PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)==
-                    PackageManager.PERMISSION_GRANTED){
+
+    public void createGoogleApiClient() {
+        mGoogleApiCleint = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiCleint.connect();
+    }
+
+    private void getLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
                 PERMISSION_GRANTED = true;
+                mMap.setMyLocationEnabled(true);
 
-            }else{
+
+
+            } else {
                 ActivityCompat.requestPermissions(UploadLocationLawyer.this,
                         permissoins
-                        ,LOCATION_PERMISSION_REQUEST);
+                        , LOCATION_PERMISSION_REQUEST);
             }
 
-        }else{
+        } else {
             ActivityCompat.requestPermissions(UploadLocationLawyer.this,
                     permissoins
-                    ,LOCATION_PERMISSION_REQUEST);
+                    , LOCATION_PERMISSION_REQUEST);
         }
-   }
+    }
 
     @Override
     public void onRequestPermissionsResult
             (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         PERMISSION_GRANTED = false;
-        switch (requestCode)
-        {
-            case LOCATION_PERMISSION_REQUEST:{
-                if(grantResults.length>0)
-                {
-                    for(int i =0;i<grantResults.length;i++)
-                    {
-                        if(grantResults[i]!=PackageManager.PERMISSION_GRANTED)
-                        {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             PERMISSION_GRANTED = false;
                             break;
                         }
                     }
-                    PERMISSION_GRANTED=true;
-                    initMap();
+                    PERMISSION_GRANTED = true;
+                    getLocationPermission();
+
                 }
             }
 
 
         }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        createGoogleApiClient();
+        getLocationPermission();
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location == null) {
+            Toast.makeText(getApplicationContext(), "Please Turn On Your GPS", Toast.LENGTH_SHORT).show();
+        } else {
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+            mMap.animateCamera(cameraUpdate);
+        }
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // mLocationRequest.setInterval(1000);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+           ActivityCompat.requestPermissions(UploadLocationLawyer.this,permissoins,LOCATION_PERMISSION_REQUEST);
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiCleint, mLocationRequest, this);
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }

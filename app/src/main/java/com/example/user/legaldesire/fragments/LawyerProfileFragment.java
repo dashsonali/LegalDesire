69f3081bfd86b7b073a5d33453f72d862f994371
rakeswain <swain.rakesh131@gmail.com>
2019-01-25 11:57:55 +0530

    LAWYER PROFILE EDITED COMPLETE!

--- app/src/main/java/com/example/user/legaldesire/fragments/LawyerProfileFragment.java
+++ app/src/main/java/com/example/user/legaldesire/fragments/LawyerProfileFragment.java
@@ -1,12 +1,21 @@
 package com.example.user.legaldesire.fragments;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
@@ -27,7 +36,9 @@ import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.example.user.legaldesire.LoginActivity;
+import com.example.user.legaldesire.MainActivity;
 import com.example.user.legaldesire.R;
+import com.example.user.legaldesire.RegistrationActivity;
 import com.example.user.legaldesire.UploadLocationLawyer;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -40,17 +51,25 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageException;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class LawyerProfileFragment extends Fragment{
 
-
+    private static final int RESULT_LOAD_IMAGE = 100;
     private  TextView name,email,areaOfPractice,phone,address,feeTxt,usersRated;
     SharedPreferences sharedPreferences;
     private RatingBar ratingBar;
     private ImageView user_menu,propic;
     ProgressBar mProgressBar;
     private Context mContext;
+    Uri selectImage;
+
     ProgressDialog mProgressDialog;
     public LawyerProfileFragment() {
         // Required empty public constructor
@@ -75,6 +94,13 @@ public class LawyerProfileFragment extends Fragment{
         user_menu = view.findViewById(R.id.user_menu);
         address = view.findViewById(R.id.addressTxt);
         propic = view.findViewById(R.id.profilePic);
+        propic.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                updateProfilePicMenu(view);
+                return true;
+            }
+        });
         mProgressBar = view.findViewById(R.id.profilePicProgresspar);
         sharedPreferences = mContext.getSharedPreferences("MyPref",Context.MODE_PRIVATE);
         name.setText(sharedPreferences.getString("name",null));
@@ -96,6 +122,28 @@ public class LawyerProfileFragment extends Fragment{
             }
         });
         return view;
+    }
+    public void updateProfilePicMenu(View view){
+        PopupMenu popupMenu = new PopupMenu(mContext,view);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if(id == R.id.uploadProfilePic)
+                {if(isReadStoragePermissionGranted())
+                {
+                    uploadImage();
+                }
+
+                    return true;
+                }
+                return false;
+            }
+        });
+        popupMenu.inflate(R.menu.upload_profile_pic);
+        popupMenu.show();
+
+
     }
     public void loadingRating(){
         mProgressDialog.setMessage("Fetching data...");
@@ -206,4 +254,94 @@ public class LawyerProfileFragment extends Fragment{
         mContext = context;
         
     }
+    public boolean isReadStoragePermissionGranted(){
+        if(Build.VERSION.SDK_INT>=23)
+        {
+            if(mContext.checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(mContext, "Permission Granted", Toast.LENGTH_SHORT).show();
+                return true;
+            }else{
+                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}
+                        ,3);
+                return  false;
+            }
+
+        }else{
+            return true;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==3&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        {
+            uploadImage();
+        }
+    }
+    public void uploadImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode ==RESULT_LOAD_IMAGE&&resultCode==RESULT_OK&&data!=null)
+        {
+            Toast.makeText(mContext,"Got Data",Toast.LENGTH_SHORT).show();
+            selectImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = mContext.getContentResolver().query(selectImage,filePathColumn,
+                    null,null,null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+           propic.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+           if(selectImage!=null)
+           {
+               try {
+                   Bitmap selectedBitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver() ,selectImage);
+                   compressAndUpload(selectedBitmap);
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }
+
+
+
+        }
+    }
+    public void compressAndUpload(Bitmap bitmap)
+    {
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mProgressDialog.setMessage("Uploading Profile Pic ...");
+        bitmap = Bitmap.createScaledBitmap(bitmap, 300 ,300, true);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 30, baos);
+        byte[] data = baos.toByteArray();
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("ProfileImages/Lawyers").child(mAuth.getCurrentUser().getEmail());
+        UploadTask uploadTask = storageReference.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                mProgressDialog.dismiss();
+                Toast.makeText(mContext,"Something Went Wrong!",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                mProgressDialog.dismiss();
+//                Intent intent = new Intent(mContext, MainActivity.class);
+//                intent.putExtra("lawyer_mail",mAuth.getCurrentUser().getEmail());
+//                startActivity(intent);
+                // ...
+            }
+        });
+
+    }
+
 }

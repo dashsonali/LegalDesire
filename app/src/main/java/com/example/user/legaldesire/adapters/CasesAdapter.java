6b0d3785441fe23db527a6b83727403a049e77ce
rakeswain <swain.rakesh131@gmail.com>
2019-01-31 11:48:30 +0530

    UPDATE AND REMOVE BUTTON

--- app/src/main/java/com/example/user/legaldesire/adapters/CasesAdapter.java
+++ app/src/main/java/com/example/user/legaldesire/adapters/CasesAdapter.java
@@ -2,9 +2,12 @@ package com.example.user.legaldesire.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.user.legaldesire.R;
 import com.example.user.legaldesire.models.CasesModel;
 
 import java.util.List;
@@ -18,11 +21,20 @@ public class CasesAdapter extends RecyclerView.Adapter<CasesAdapter.MyViewHolder
     }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cases_cell,parent,false);
+
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+        final CasesModel currentCase = itemList.get(position);
+        holder.caseName.setText(currentCase.getCasename());
+        holder.courtName.setText("Court: "+currentCase.getCourt());
+        holder.lawyer.setText("Lawyer: "+currentCase.getLawyer());
+        holder.initiationdate.setText("Initiation Date: "+currentCase.getDate_of_initation());
+        holder.nextdate.setText("Next Date: "+currentCase.getNext_date());
+
 
     }
 
@@ -32,9 +44,16 @@ public class CasesAdapter extends RecyclerView.Adapter<CasesAdapter.MyViewHolder
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-
+        TextView courtName,caseName,opposition_party,initiationdate,nextdate,lawyer;
         public MyViewHolder(View itemView) {
             super(itemView);
+            courtName = itemView.findViewById(R.id.court_name);
+            caseName = itemView.findViewById(R.id.case_name);
+            opposition_party = itemView.findViewById(R.id.oppositon_party);
+            initiationdate = itemView.findViewById(R.id.initiation_date);
+            nextdate =itemView.findViewById(R.id.next_date);
+            lawyer = itemView.findViewById(R.id.lawyer_name);
+
         }
     }
 }
